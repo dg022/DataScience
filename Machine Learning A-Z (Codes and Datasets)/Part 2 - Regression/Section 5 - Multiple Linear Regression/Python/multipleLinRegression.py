176327a40b71da45d5b18334c7b5bb886fe07501
David George <44826125+dg022@users.noreply.github.com>
2021-04-21 17:43:22 -0600

    standarizing data

--- Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/multipleLinRegression.py
+++ Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/multipleLinRegression.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from  sklearn.preprocessing import StandardScaler
 
 # Importing the dataset
 data = pd.read_csv('Data.csv')
@@ -14,3 +16,12 @@ ct  = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remain
 x = np.array(ct.fit_transform(x))
 
 x_train, x_test, y_train, y_test  = train_test_split(x, y, test_size=0.2,  random_state=1)
+
+# Preprocessing of the data is now complete, we now need to do feature scaling
+
+sc = StandardScaler()
+x_train[:, :3] = sc.fit_transform(x_train[:, :3])
+x_test[:, :3 ] = sc.transform(x_test[:, :3])
+
+#Not enteraly sure about what to do with y_triana dn y_test, do we want to change those
+#probably not, because we want that value to give an estimate.
