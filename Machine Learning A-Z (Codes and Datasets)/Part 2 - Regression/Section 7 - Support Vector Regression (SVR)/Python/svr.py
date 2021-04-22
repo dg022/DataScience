016211fc46bfbcccaacafa12a580e9c47e0d7af8
David George <44826125+dg022@users.noreply.github.com>
2021-04-22 16:15:44 -0600

    support vector regression

---
+++ Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/Python/svr.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import PolynomialFeatures
+from  sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+sc = StandardScaler()
+sc_y = StandardScaler()
+#the goal of standarisation is so the features stay in the same range
+#you cant to do it to dumym varibles, and you wont be able to itnrepert the categorial varibles
+
+#dont apply feature scalling to categorical varibles
+
+#feature scaling for the levels
+x = sc.fit_transform(x)
+y = y.reshape(len(y),1)
+y = sc_y.fit_transform(y)
+
+regressor  = SVR(kernel='rbf')
+
+regressor.fit(x, y)
+r = sc_y.inverse_transform(regressor.predict(sc.transform([[6.5]])))
+print(r)
